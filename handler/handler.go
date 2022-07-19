778b98f43a63cc446c6e21c1db331f9a70652b79
kakukaku-15 <kakusika301@gmail.com>
2022-07-19 18:42:47 +0900

    fix hello world
    Signed-off-by: kakukaku-15 <kakusika301@gmail.com>

--- handler/handler.go
+++ handler/handler.go
@@ -5,7 +5,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func helloworld() echo.HandlerFunc {
+func Helloworld() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	}
